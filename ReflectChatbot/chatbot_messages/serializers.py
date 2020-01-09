3498add2919783a49fdb1c1d5abb8edfdcd0d71f
mattchapleau <36090409+mattchapleau@users.noreply.github.com>
2020-01-08 19:26:08 -0500

    Improvements to UI and webhosting

--- ReflectChatbot/chatbot_messages/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from chatbot_messages.models import chatbot_message
-
-# message serializer
-class ChatbotMessageSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = chatbot_message
-        fields = '__all__'
